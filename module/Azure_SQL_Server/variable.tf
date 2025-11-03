bec27689bbfbbea61ffd0139e62390723282ac2c
Bishal Kumar Das <BishalDasDevops@gmail.com>
2025-11-03 16:36:13 +0530

    Update variable.tf

--- module/Azure_SQL_Server/variable.tf
+++ module/Azure_SQL_Server/variable.tf
@@ -1,12 +1,3 @@
 variable "sql_server" {
-    # description = "Details of the SQL Server"
-    # type = map(object({
-    #     name                         = string
-    #     resource_group_name          = string
-    #     location                     = string
-    #     version                      = string
-    #     administrator_login          = string
-    #     administrator_login_password = string
-    # }))
   
 }
